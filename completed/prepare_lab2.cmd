a18401b58c3b00ecf7ca78400d99ecc9cfa2278f
Alexander Kryukov <akryukov@luxoft.com>
2016-09-14 15:23:51 +0600

    Упрощены скрипты для подготовки ответов.

--- completed/prepare_lab2.cmd
+++ completed/prepare_lab2.cmd
@@ -6,11 +6,7 @@ for %%f in (lab02*.html) do del %%~nf.html
 cd ../completed
 
 for %%f in (lab02*.sql) do (
-psql -d human_resources -U postgres -f %%~nf.sql -H -F "," > %%~nf.raw
-echo ^<html^>^<head^>^<meta charset='utf-8'/^>^</head^>^<body^> > %%~nf.html
-type %%~nf.raw >> %%~nf.html
-echo ^</body^>^</html^> >> %%~nf.html
-del %%~nf.raw
-copy "%%~nf.html" "../results/%%~nf.html"
-del %%~nf.html
+echo ^<html^>^<head^>^<meta charset='utf-8'/^>^</head^>^<body^> > ../results/%%~nf.html
+psql -d human_resources -U postgres -f %%~nf.sql -H -F "," >> ../results/%%~nf.html
+echo ^</body^>^</html^> >> ../results/%%~nf.html
 )
